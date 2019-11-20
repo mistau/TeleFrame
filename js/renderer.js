99a9b5a8ace9be0e69a53291877feefece795504
Michael Staudenmaier <michael@staudis.net>
2019-11-20 16:29:20 +0100

    Approach to keep config file compatible with previous version. Old variable playSoundOnRecieve still works in parallel to the new variable playSoundOnRecieve.

--- js/renderer.js
+++ js/renderer.js
@@ -18,9 +18,18 @@ var container = document.getElementById("container");
 var isPaused = false;
 var currentImageIndex = images.length;
 var startTime, endTime, longpress, timeout, recordSwal, currentChatId, currentMessageId, currentTimeout;
+var playSoundOnReceive = false;
+
+// deal with both config variables to stay compatible
+if ( (config.hasOwnProperty(playSoundOnReceive) &&
+      config.playSoundOnReceive)
+   || (config.hasOwnProperty(playSoundOnRecieve) &&
+      config.playSoundOnRecieve )) {
+   playSoundOnReceive = true;
+}
 
 // configure sound notification sound
-if (config.playSoundOnReceive != false) {
+if(playSoundOnReceive) {
   var audio = new Audio(__dirname + "/sound1.mp3");
 }
 
@@ -114,7 +123,7 @@ ipcRenderer.on("recordError", function(event, arg) {
 // handle new incoming image
 ipcRenderer.on("newImage", function(event, arg) {
   newImage(arg.sender, arg.type);
-  if (config.playSoundOnReceive != false) {
+  if (config.playSoundOnReceive) {
     audio.play();
   }
 });
